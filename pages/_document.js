558fed0c1fbcc748a713068726d0b6caa04812b3
rdevz-ph <brosasromel01@gmail.com>
2025-07-09 04:57:04 +0800

    add GoatCounter

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <script
+          data-goatcounter="https://rdevz-ph.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
