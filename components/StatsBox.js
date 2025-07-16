55c05bef37aa77b1e517ba39bdf9f976401b7bde
rdevz-ph <brosasromel01@gmail.com>
2025-07-16 12:15:25 +0800

    remove unnecessary codes

--- components/StatsBox.js
+++ components/StatsBox.js
@@ -43,7 +43,7 @@ export default function StatsBox({ settings }) {
             <StatCard
                 icon={<EyeIcon className="w-6 h-6" />}
                 label="Total Visitors"
-                value={settings.visitor_count.toLocaleString('en-US')}
+                value={settings.visitor_count}
                 color="purple"
                 aos="fade-up"
                 delay="100"
