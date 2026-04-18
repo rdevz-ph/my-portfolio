73aad07601701fc79a7e3fa1e4965215b1668cf6
rdevz-ph <brosasromel01@gmail.com>
2026-04-18 19:36:25 +0800

    fix: resolve mobile layout misalignment by locking viewport zoom and overflow

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {/* Essential Meta Tags - These should be in _app.js */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
